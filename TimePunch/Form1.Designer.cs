e245f125c0095a6f4658ab68395901309b561a20
P13Darksight <g_m_nunez@live.com>
2014-04-21 13:46:39 -0400

    - Initial Commit and stable release

---
+++ TimePunch/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TimePunch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.punchButton = new System.Windows.Forms.Button();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.howLongLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.recalcButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rateNumeric = new System.Windows.Forms.NumericUpDown();
+            this.earnedLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rateNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // punchButton
+            // 
+            this.punchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.punchButton.Location = new System.Drawing.Point(41, 182);
+            this.punchButton.Name = "punchButton";
+            this.punchButton.Size = new System.Drawing.Size(75, 23);
+            this.punchButton.TabIndex = 0;
+            this.punchButton.Text = "Punch";
+            this.punchButton.UseVisualStyleBackColor = true;
+            this.punchButton.Click += new System.EventHandler(this.punchButton_Click);
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Location = new System.Drawing.Point(38, 30);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(32, 13);
+            this.startLabel.TabIndex = 1;
+            this.startLabel.Text = "Start:";
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Location = new System.Drawing.Point(38, 60);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(29, 13);
+            this.endLabel.TabIndex = 2;
+            this.endLabel.Text = "End:";
+            // 
+            // howLongLabel
+            // 
+            this.howLongLabel.AutoSize = true;
+            this.howLongLabel.Location = new System.Drawing.Point(38, 86);
+            this.howLongLabel.Name = "howLongLabel";
+            this.howLongLabel.Size = new System.Drawing.Size(62, 13);
+            this.howLongLabel.TabIndex = 3;
+            this.howLongLabel.Text = "How Long?";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveButton.Location = new System.Drawing.Point(203, 182);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.loadButton.Location = new System.Drawing.Point(284, 182);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 5;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // recalcButton
+            // 
+            this.recalcButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.recalcButton.Location = new System.Drawing.Point(122, 182);
+            this.recalcButton.Name = "recalcButton";
+            this.recalcButton.Size = new System.Drawing.Size(75, 23);
+            this.recalcButton.TabIndex = 6;
+            this.recalcButton.Text = "ReCalc";
+            this.recalcButton.UseVisualStyleBackColor = true;
+            this.recalcButton.Click += new System.EventHandler(this.recalcButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "At an hourly rate of:";
+            // 
+            // rateNumeric
+            // 
+            this.rateNumeric.DecimalPlaces = 2;
+            this.rateNumeric.Location = new System.Drawing.Point(143, 110);
+            this.rateNumeric.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.rateNumeric.Name = "rateNumeric";
+            this.rateNumeric.Size = new System.Drawing.Size(96, 20);
+            this.rateNumeric.TabIndex = 8;
+            // 
+            // earnedLabel
+            // 
+            this.earnedLabel.AutoSize = true;
+            this.earnedLabel.Location = new System.Drawing.Point(38, 146);
+            this.earnedLabel.Name = "earnedLabel";
+            this.earnedLabel.Size = new System.Drawing.Size(104, 13);
+            this.earnedLabel.TabIndex = 9;
+            this.earnedLabel.Text = "You would of made: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 217);
+            this.Controls.Add(this.earnedLabel);
+            this.Controls.Add(this.rateNumeric);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recalcButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.howLongLabel);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.punchButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Time Punch";
+            ((System.ComponentModel.ISupportInitialize)(this.rateNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button punchButton;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.Label howLongLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button recalcButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown rateNumeric;
+        private System.Windows.Forms.Label earnedLabel;
+    }
+}
+
---
+++ TimePunch/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace TimePunch
+{
+    public partial class Form1 : Form
+    {
+        private DateTime firstPunch;
+        private DateTime lastPunch;
+     
+        public Form1()
+        {
+            firstPunch = DateTime.MinValue;
+            lastPunch = DateTime.MinValue;
+            InitializeComponent();
+            
+        }
+        private void Calculate()
+        {
+            TimeSpan diff = lastPunch - firstPunch;
+            howLongLabel.Text = string.Format("How Long? {0} Days, {1} Hours , {2} Minutes, {3} Seconds",
+                diff.Days, diff.Hours, diff.Minutes, diff.Seconds);
+            decimal rate = rateNumeric.Value;
+            decimal totalHours = new decimal(diff.TotalHours);
+            decimal amountEarned = rate * totalHours;
+            earnedLabel.Text = string.Format("You earned: {0}", Math.Round(amountEarned,2));
+
+        }
+        private void punchButton_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            if (firstPunch == DateTime.MinValue)
+            {
+                firstPunch = DateTime.Now;
+                startLabel.Text = string.Format("Start: {0}", firstPunch);
+            }
+            else
+            {
+                lastPunch = DateTime.Now;
+                endLabel.Text = string.Format("End: {0}",lastPunch);
+                punchButton.Enabled = false;
+                Calculate();
+
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+        
+        private void SavePunchFile()
+        {
+            this.Cursor = Cursors.WaitCursor;
+            using (SaveFileDialog dialog = new SaveFileDialog())
+            {
+                dialog.Filter = "Time Sheet (*.tsheet) |*.tsheet|All Files|*.*";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(dialog.FileName))
+                    {
+                        writer.WriteLine(firstPunch.ToString());
+                        writer.WriteLine(lastPunch.ToString());
+                        writer.Close();
+                    }
+                }
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+        private void LoadPunchFile()
+        {
+            this.Cursor = Cursors.WaitCursor;
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Time Sheet (*.tsheet) |*.tsheet|All Files|*.*";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader reader = new StreamReader(dialog.FileName))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            try
+                            {
+                                if (firstPunch == DateTime.MinValue)
+                                    firstPunch = DateTime.Parse(reader.ReadLine());
+                                else
+                                    lastPunch = DateTime.Parse(reader.ReadLine());
+                            }
+                            catch { }
+                        }
+                    }
+                    if (firstPunch != DateTime.MinValue)
+                        startLabel.Text = string.Format("Start: {0}", firstPunch);
+                    if (lastPunch != DateTime.MinValue)
+                        endLabel.Text = string.Format("End: {0}", lastPunch);
+                }
+            }
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SavePunchFile();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            LoadPunchFile();
+        }
+
+        private void recalcButton_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+    }
+}
